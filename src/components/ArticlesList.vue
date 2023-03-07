be2074e61ffefd6d242bee70034a28502cd21372
banetysto <banetysto@gmail.com>
2023-03-07 15:04:13 +0100

    3 files modified, 1 added, 1 deleted

---
+++ src/components/ArticlesList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container list row">
+    <h1>Articles LIST</h1>
+    <div class="col-md-6">
+      <ul class="list-group">
+        <li></li>
+      </ul>
+      <button class="m-3 btn btn-sm btn-danger">Tout Suppr.</button>
+    </div>
+    <div class="col-md-6">
+      <div>
+        <article-details></article-details>
+      </div>
+      <div>
+        <p>Veuillez selectionner un article.</p>
+      </div>
+    </div>
+  </div>
+</template>
